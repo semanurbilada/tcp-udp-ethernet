5d3e49d11810f84e21b4c3c1f7171fa6dfa41fc0
semanurbilada <semanurbilada@hotmail.com>
2024-07-26 13:57:32 +0300

    Fix test codes based on Raspberry tests

--- test/test_client.py
+++ test/test_client.py
@@ -6,7 +6,8 @@ import zlib
 def create_message(seq, end, timestamp, message):
     # Ensure the message is exactly 60 bytes
     message = message.ljust(60)[:60]
-    # Combine the components
+    # Combine the components of packets into a single message
+    # It contains sequence identifier (S and E), timestamp, fixed-length message
     data = struct.pack('!2c1b1d60s', seq, end, 0, timestamp, message.encode('utf-8'))
     # Calculate the CRC32
     crc = zlib.crc32(data) & 0xffffffff
@@ -15,9 +16,6 @@ def create_message(seq, end, timestamp, message):
 
 def udp_client(server_ip, port, message_count):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
-    port = 12345
-    server_ip = 'server_ip'
     
     try:
         for i in range(message_count):
@@ -37,3 +35,6 @@ def udp_client(server_ip, port, message_count):
     finally:
         s.close()
         print("Socket closed")
+
+# Change port, ip and message count and call the func
+udp_client(port = 12345, server_ip = 'server_ip', message_count=1000)
--- test/test_server.py
+++ test/test_server.py
@@ -1,28 +1,25 @@
 import socket
 import struct
-import time
 import zlib
 
 def udp_server(server_ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    port = 12345
-    server_ip = 'server_ip'
 
     s.bind((server_ip, port))
     print("UDP Socket successfully created and binded to port: ", port)
     
     while True:
         data, addr = s.recvfrom(1024)
-        print('Got connection from', addr)
+        print('\nGot connection from', addr)
         
-        # Unpack the data
+        # Unpack the data with sequence identifier: S and E
         seq, end, zero, timestamp, message, received_crc = struct.unpack('!2c1b1d60sI', data)
         message = message.decode('utf-8').rstrip('\x00')
 
         # Recalculate the CRC to verify the data integrity
         crc = zlib.crc32(data[:-4]) & 0xffffffff
         
+        # If CRC is correct, then print message and send response
         if crc == received_crc:
             print(f"Message from client: {message}")
             print(f"Timestamp: {timestamp}")
@@ -30,3 +27,6 @@ def udp_server(server_ip, port):
             s.sendto(response.encode('utf-8'), addr)
         else:
             print("CRC check failed. Data may be corrupted.")
+
+# Change port, ip and call the func
+udp_server(port = 12345, server_ip = 'server_ip')
