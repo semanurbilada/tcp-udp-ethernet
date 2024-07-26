7c267718d1a99bfd6ef9cad2c8fa433f193cc8ae
semanurbilada <semanurbilada@hotmail.com>
2024-07-26 14:24:17 +0300

    Add frequency calc to test codes based on Raspberry tests

--- test/test_server.py
+++ test/test_server.py
@@ -1,13 +1,15 @@
 import socket
 import struct
 import zlib
+import time
 
 def udp_server(server_ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
     s.bind((server_ip, port))
-    print("UDP Socket successfully created and binded to port: ", port)
+    print("UDP Socket successfully created and binded to port", port)
     
+    timestamps = []
+
     while True:
         data, addr = s.recvfrom(1024)
         print('\nGot connection from', addr)
@@ -22,7 +24,19 @@ def udp_server(server_ip, port):
         # If CRC is correct, then print message and send response
         if crc == received_crc:
             print(f"Message from client: {message}")
-            print(f"Timestamp: {timestamp}")
+            print(f"Sent Timestamp: {timestamp}")
+            
+            # Record the timestamp when the server receives the message
+            received_time = time.time()
+            timestamps.append(received_time)
+            
+            # Calculate and print the frequency if we have at least two timestamps
+            if len(timestamps) > 1:
+                time_differences = [t2 - t1 for t1, t2 in zip(timestamps[:-1], timestamps[1:])]
+                avg_time_difference = sum(time_differences) / len(time_differences)
+                frequency = 1 / avg_time_difference
+                print(f"Frequency: {frequency:.2f} Hz")
+            
             response = 'Thank you for connecting!'
             s.sendto(response.encode('utf-8'), addr)
         else:
